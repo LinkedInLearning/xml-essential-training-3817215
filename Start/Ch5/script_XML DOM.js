3e46b6b8e3bf310fdb44bcd0d227df18655c9656
jagb40 <jg.ndongendeba@gmail.com>
2024-06-26 18:20:12 +0200

    End of tuto

--- Start/Ch5/script_XML DOM.js
+++ Start/Ch5/script_XML DOM.js
@@ -1,24 +1,37 @@
-// remove the first <book> element from the loaded xml:
+//  copies the first <book> node and appends it to the root node of the document:
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
+        myFunction(this)
     }
 }
-xhttp.open("GET", "books.xml", true);
+xhttp.open("GET", "books.xml",true);
 xhttp.send();
 
 function myFunction(xml) {
-    var text ="";
+    var text = "";
     var xmlDoc = xml.responseXML;
-    var x = xmlDoc.getElementsByTagName("book");
-    var xLen = x.length;
-    var child = xmlDoc.documentElement.childNodes[1];
-    var childRemoved = child.parentNode.removeChild(child);
-    var nxLen = x.length;
-    console.log(childRemoved);
-    // document.getElementById("demo").innerHTML = ;
+    var x = xmlDoc.documentElement;
+    var y = xmlDoc.getElementsByTagName("book")[0];
+    var newNode = y.cloneNode(true);
+    x.appendChild(newNode);
+    var z = xmlDoc.getElementsByTagName("title");
+    var zLen = z.length;
+    for (let i = 0; i < zLen; i++) {
+        text += `${z[i].childNodes[0].nodeValue}<br>`;
+        
+    }
 
+
+    console.log(text);
+    document.getElementById("demo").innerHTML = text;
+
+
+    
+    
 }
+
+
+
 
